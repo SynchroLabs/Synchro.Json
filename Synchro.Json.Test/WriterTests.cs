95b420586d0e48390c8c37a921007ea76ec3e7c4
Blake Ramsdell <blake@synchro.io>
2014-10-26 22:39:49 -0700

    First cut of Writer support.

---
+++ Synchro.Json.Test/WriterTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using System.IO;
+
+namespace Synchro.Json.Test
+{
+	[TestFixture()]
+	public class WriterTests
+	{
+		private static void TestRoundtrip(string json)
+		{
+			object parsedObject = Parser.ParseValue(new StringReader(json));
+			var writer = new StringWriter();
+
+			Writer.WriteValue(writer, parsedObject);
+
+			Assert.AreEqual(json, writer.ToString());
+		}
+
+		[Test()]
+		public void TestWriteValue()
+		{
+			TestRoundtrip("{\"foo\":0,\"bar\":\"kitty\",\"baz\":[8,\"dog\"]}");
+		}
+	}
+}
+
---
+++ Writer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+
+namespace Synchro.Json
+{
+	public class Writer
+	{
+		private static void WriteString(TextWriter writer, string _string)
+		{
+			writer.Write('\"');
+			writer.Write(_string);
+			writer.Write('\"');
+		}
+
+		private static void WriteNumber(TextWriter writer, int i)
+		{
+			writer.Write(i);
+		}
+
+		private static void WriteArray(TextWriter writer, object[] array)
+		{
+			bool firstElement = true;
+
+			writer.Write('[');
+			foreach (var value in array)
+			{
+				if (!firstElement)
+				{
+					writer.Write(',');
+				}
+				else
+				{
+					firstElement = false;
+				}
+
+				WriteValue(writer, value);
+			}
+			writer.Write(']');
+		}
+
+		public static void WriteValue(TextWriter writer, object value)
+		{
+			if (value.GetType() == typeof(JsonObject))
+			{
+				WriteObject(writer, (JsonObject)value);
+			}
+			else if (value.GetType() == typeof(string))
+			{
+				WriteString(writer, (string)value);
+			}
+			else if (value.GetType() == typeof(int))
+			{
+				WriteNumber(writer, (int)value);
+			}
+			else if (value.GetType() == typeof(object[]))
+			{
+				WriteArray(writer, (object[])value);
+			}
+			else
+			{
+				throw new IOException(string.Format("Unknown object type {0}", value.GetType()));
+			}
+		}
+
+		private static void WriteObject(TextWriter writer, JsonObject _object)
+		{
+			bool firstKey = true;
+
+			writer.Write('{');
+			foreach (var key in _object.Keys)
+			{
+				var value = _object[key];
+
+				if (!firstKey)
+				{
+					writer.Write(',');
+				}
+				else
+				{
+					firstKey = false;
+				}
+
+				WriteString(writer, key);
+
+				writer.Write(':');
+
+				WriteValue(writer, value);
+			}
+			writer.Write('}');
+		}
+	}
+}
+
